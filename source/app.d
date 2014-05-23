153cf0b803715ffc7bd11ea0561cb57bf31e536c
Etienne Cimon <etcimon@gmail.com>
2014-05-23 10:10:06 -0400

    first commit

---
+++ source/app.d
@@ -0,0 +1,77 @@
+﻿module test;
+
+import std.stdio;
+import std.datetime;
+import std.typecons;
+import std.conv;
+
+string[] gcMem;
+StopWatch total;
+
+alias Report = Tuple!(string, "descr", StopWatch, "sw", long, "msecs");
+Report[] linearAddDel(int[] dataSz, int times = 100_000){
+
+	Report[] reports;
+	foreach (del; [false, true]){
+		foreach (i; 0..dataSz.length){
+			Report report;
+			report.descr = "[" ~ dataSz[i].to!string ~ "B; " ~ times.to!string ~ ";";
+			total.start();
+			report.sw.start();
+			
+			if (!del)
+				foreach (j; 0..times)
+					gcMem ~= new string(dataSz[i]);
+			
+			else
+				foreach (j; 0..times)
+					delete gcMem[i];
+			
+			report.sw.stop();
+			total.stop();
+			report.msecs = report.sw.peek().msecs;
+			report.descr ~= report.msecs.to!string ~ "]";
+			reports ~= report;
+		}
+	}
+	return reports;
+}
+
+void main(){
+
+	int[] dataSz = [10, 20, 40, 100, 200, 400, 1000, 10_000, 20_000];
+	int times = 100_000;
+	Report[][] reportCollections; // [ [ addReport, delReport ] , ... ]
+	foreach (i; 0..4)
+		reportCollections ~= linearAddDel(dataSz, times);
+
+	string[] diffDescr;
+	int i;
+	foreach (bytes; dataSz){
+		foreach (j; 0..reportCollections.length - 1){
+			long diff = reportCollections[j + 1][i].msecs - reportCollections[j][i].msecs;
+			float perc = ((reportCollections[j + 1][i].msecs.to!float - reportCollections[j][i].msecs.to!float)/reportCollections[j][i].msecs.to!float*100f).round(2);
+			string descr = "[" ~ bytes.to!string ~ "B; ";
+			if (diff > 0)
+				descr ~= "+";
+			descr ~= diff.to!string ~ "ms; ";
+			if (diff > 0)
+				descr ~= "+";
+			descr ~= perc.to!string ~ "%]";
+			diffDescr ~= descr;
+		}
+
+		i++;
+	}
+	writeln("Differences of time it took between first and second runs of allocations: ");
+	foreach (descr; diffDescr)
+		writeln(descr);
+	writeln("Total test took: " ~ total.peek().msecs.to!string ~ " ms");
+}
+
+import std.math;
+float round(float x, uint places)
+{
+	float pwr = pow(10.0, places);
+	return std.math.round(x * pwr) / pwr;
+}
