4768b22c023fdf57835b9aaef3cf4838c9b16453
Etienne Cimon <etcimon@gmail.com>
2014-05-28 13:53:39 -0400

    modified:   dub.userprefs 	modified:   source/app.d

--- source/app.d
+++ source/app.d
@@ -161,26 +161,28 @@ Report[] mixedManualArrays(int[] dataSz, int times = 100_000){
 
 
 void main(){
-	cast(void)GC.stats(); // start stats
-	int[] dataSz = [10000, 20000, 30000];
-	int times = 10_000;
+	GCStats* stats = new GCStats();
+	stats.samplingMaxDiscrepancy = 8;
+	stats.disableSampling = false; 
+	GC.stats(stats);
+	GC.stats(stats); 
+	int[] dataSz = [10, 20, 30, 50, 100, 300, 600]; 
+	int times = 100_000; 
 	Report[][] reportCollections; // [ [ addReport, delReport ] , ... ]
-
-	version(ManualMemory){
-		reportCollections ~= manualAddDel(dataSz, times);
-		reportCollections ~= manualAddDel(dataSz, times);
+	version(ManualMemory){ 
+		reportCollections ~= manualAddDel(dataSz, times); 
+		reportCollections ~= manualAddDel(dataSz, times); 
 		reportCollections ~= mixedManualArrays(dataSz, times);
-		reportCollections ~= mixedManualArrays(dataSz, times);
-
-	}else {
+		reportCollections ~= mixedManualArrays(dataSz, times); 
+	}else { 
+		reportCollections ~= linearAddDel(dataSz, times); 
 		reportCollections ~= linearAddDel(dataSz, times);
 		reportCollections ~= linearAddDel(dataSz, times);
-		reportCollections ~= mixedLinearAddDel(dataSz, times);
-		reportCollections ~= mixedLinearAddDel(dataSz, times);
-	}
-	string[] diffDescr;
-	int i;
-	foreach (bytes; dataSz){
+		reportCollections ~= linearAddDel(dataSz, times); 
+	} 
+	string[] diffDescr; 
+	int i; 
+	foreach (bytes; dataSz){ 
 		foreach (j; 0..reportCollections.length - 1){
 			long diff = reportCollections[j + 1][i].msecs - reportCollections[j][i].msecs;
 			float perc = ((reportCollections[j + 1][i].msecs.to!float - reportCollections[j][i].msecs.to!float)/reportCollections[j][i].msecs.to!float*100f).round(2);
@@ -201,12 +203,24 @@ void main(){
 		writeln(descr);
 	writeln("Total test took: " ~ total.peek().msecs.to!string ~ " ms");
 
-	GCStats stats = GC.stats();
-	writeln(stats.freed, " freed");
-	writeln(stats.used, " used");
-	writeln(stats.collections, " collections");
-	writeln(size_t.max, " max size_t");
-	writeln((stats.freed.to!float/stats.used.to!float)*100f, " avg freed % per collection");
+	GC.stats(stats);
+	writeln(stats.samplingSamples, " samples");
+	writeln(stats.samplingPopulation, " population");
+	writeln(stats.totalSamplingFailures, " total sampling failures");
+	writeln(stats.totalSamplings, " total samplings");
+	writeln(stats.elapsedInSampling.msecs, " ms in sampling");
+	writeln(stats.bytesFreedInCollections, " freed");
+	writeln(stats.bytesUsedInCollections, " used"); 
+	writeln(stats.elapsedInCollections.msecs, " ms in collections"); 
+	writeln(stats.elapsedInBigAllocations.msecs, " ms in big allocations"); 
+	writeln(stats.elapsedInSmallAllocations.msecs, " ms in small allocations");
+	writeln(stats.elapsedInFreeToOS.usecs, " us in minimize");
+	writeln(stats.elapsed.msecs, " ms GC lived");
+	writeln(stats.bytesFreedToOS, "B Freed to OS");
+	writeln(stats.totalFreeToOS, " total Freed to OS");  
+	writeln(stats.totalCollections, " collections");
+	writeln(1543154/2123);
+	writeln((stats.bytesFreedInCollections.to!float/stats.bytesUsedInCollections.to!float)*100f, " avg freed % per collection");
 }
 
 import std.math;
